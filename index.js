5e7c71c59895c06963686b7bfb854d76ebe8b522
tirahman105 <tirahman105@gmail.com>
2023-05-16 09:27:39 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -26,6 +27,22 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+      return res.status(401).send({error: true, message: 'unauthorized access'});
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+      if(err){
+          return res.status(401).send({error: true, message: 'unauthorized access'})
+      }
+      req.decoded = decoded;
+      next();
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -43,6 +60,20 @@ async function run() {
     })
 
 
+    // jwt
+    app.post('/jwt', (req, res)=> {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+      console.log(token)
+      res.send({token})
+    })
+
+
+
+
+    // services routes
+
     app.get('/services/:id', async(req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId (id)}
@@ -58,8 +89,17 @@ async function run() {
 
     // bookings
 
-    app.get('/bookings', async(req, res) => {
-      console.log(req.query.email);
+    app.get('/bookings',verifyJWT, async(req, res) => {
+      // console.log(req.headers.authorization);
+
+      const decoded = req.decoded;
+      console.log('came back after verify', decoded)
+
+      if(decoded.email !== req.query.email){
+          return res.status(403).send({error: 1, message: 'forbidden access'})
+      }
+
+      
       let query = {};
       if(req.query?.email){
         query = {email: req.query.email}
